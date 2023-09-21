933a1cf346bdee59fbd5cb35c3ece4dc1584bec9
MazonHenrique <hmazon87@yahoo.com>
2023-09-20 21:17:32 -0300

    Organizado test com Arrange, Act, Assert

--- src/test/java/br/com/henrique/math/SimpleMathTest.java
+++ src/test/java/br/com/henrique/math/SimpleMathTest.java
@@ -1,28 +1,38 @@
 package br.com.henrique.math;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("Test Math operations in SimpliMathTest")
 public class SimpleMathTest {
    
 	@Test
+	@DisplayName("Test 6.2 + 2 = 8.2")
     void testSum() {
+		//AAA Arrange, Act, Assert
 		
-        SimpleMath math = new SimpleMath();
+		//Given / Arrange 
+		SimpleMath math = new SimpleMath();
         
-        //teste da funcao sum
         double firstNumber = 6.2D;
 		double secondNumber = 2D;
+		double expected = 8.2D;
+        
+		//When / Act
 		Double actual = math.sum(firstNumber, secondNumber);
-        double expected = 8.2D;
-        assertEquals(expected, actual, 
+        
+		//Then / Assert
+		assertEquals(expected, actual, 
         		() -> firstNumber + " + " + secondNumber + " did not produce " + expected);
  	}
 	
 	@Test
+	@DisplayName("Test 6.2 - 2 = 4.2")
     void testSubtraction() {
 		
+		//
         SimpleMath math = new SimpleMath();
         
         //teste da funcao subtração
@@ -35,6 +45,7 @@ public class SimpleMathTest {
  	}
 
 	@Test
+	@DisplayName("Test 6 * 2 = 12")
     void testMultiplication() {
 		
         SimpleMath math = new SimpleMath();
@@ -49,6 +60,7 @@ public class SimpleMathTest {
  	}
 
 	@Test
+	@DisplayName("Test 10  /  2 = 5")
     void testDivision() {
 		
         SimpleMath math = new SimpleMath();
@@ -63,6 +75,7 @@ public class SimpleMathTest {
  	}	
 
 	@Test
+	@DisplayName("Test mean 10 and 2 = 6")
     void testMean() {
 		
         SimpleMath math = new SimpleMath();
@@ -77,11 +90,12 @@ public class SimpleMathTest {
  	}	
 	
 	@Test
+	@DisplayName("Test square with 81 = 9")
     void testSquareRoot() {
 		
         SimpleMath math = new SimpleMath();
         
-        //teste da funcao meia
+        //teste da funcao square
         double firstNumber = 81;
 		Double actual = math.squareRoot(firstNumber);
         double expected = 9D;
